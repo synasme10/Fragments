ff882aca999c99cc43efa9ccb2c7650c38e6fd8b
Softwarica College Lab <softwarica@softwarica.com>
2019-04-23 11:00:39 +0545

    First Commit

--- app/src/main/java/com/e/fragments/MainActivity.java
+++ app/src/main/java/com/e/fragments/MainActivity.java
@@ -1,13 +1,51 @@
 package com.e.fragments;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import Fragments.FirstFragment;
+import Fragments.SecondFragment;
 
+public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button BtnFragment;
+    private Boolean status=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnFragment=findViewById(R.id.BtnFragment);
+        BtnFragment.setOnClickListener(this);
+
     }
-}
+    @Override
+    public void onClick(View v){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+        if (status){
+            FirstFragment firstFragment=new FirstFragment();
+            fragmentTransaction.add(R.id.fragmentcontainer,firstFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            BtnFragment.setText("Load Second Fragment");
+            status=false;
+
+        }
+        else{
+
+                SecondFragment secondFragment=new SecondFragment();
+                fragmentTransaction.add(R.id.fragmentcontainer,secondFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                BtnFragment.setText("Load First Fragment");
+                status=true;
+
+            }
+        }
+    }
+
